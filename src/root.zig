c68acea35ff200f5573166ffa4db35301a87f526
dload0 <170800883+dload0@users.noreply.github.com>
2024-09-26 23:11:48 +0100

    fix: Fix for `JNI_OnLoad` & added env getters.
    - Fixed type issue for `vm` within `JNI_OnLoad`.
    - Added VM creation from raw pointer.
    - Added JNIEnv getter from vm.
    
    _See:_ https://github.com/SuperIceCN/zig-jni/issues/1

--- src/root.zig
+++ src/root.zig
@@ -7,7 +7,18 @@ comptime {
 }
 
 /// This function is called when the JVM loads the native library.
-export fn JNI_OnLoad(_: *jni.JavaVM, _: ?*anyopaque) callconv(.C) jni.jint {
+export fn JNI_OnLoad(vm: *jni.cjni.JavaVM, _: ?*anyopaque) callconv(.C) jni.jint {
+    // Warp the VM into the wrapper.
+    const warped_vm = @constCast(&jni.JavaVM.warp(vm));
+
+    // JNIEnv pointer.
+    var env: jni.JNIEnv = undefined;
+
+    // Call getEnv to retrieve the JNIEnv pointer.
+    _ = warped_vm.getEnv(&env, @intFromEnum(jni.JNIVersion.JNI1_6)) catch blk: {
+        break :blk undefined;
+    };
+
     // Print debug.
     std.debug.print("[ ! ] `JNI_OnLoad` called.\n", .{});
 
