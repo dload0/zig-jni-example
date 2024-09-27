fb51c03beffb9eb61d1e5b6844a27946672f3c45
dload0 <170800883+dload0@users.noreply.github.com>
2024-09-27 15:43:07 +0100

    fix: Fix for exported native function.
    - Fixed type issue for `env` within `bootstrap`.
    - Added JNIEnv creation from raw pointer.

--- src/lib.zig
+++ src/lib.zig
@@ -2,7 +2,10 @@ const std = @import("std");
 const jni = @import("jni");
 
 /// This function is called from the Java side.
-pub fn bootstrap(_: *jni.JNIEnv, _: jni.jclass) callconv(.C) void {
+pub fn bootstrap(env: *jni.cjni.JNIEnv, _: jni.jclass) callconv(.C) void {
     // Print debug.
     std.debug.print("[ ! ] `Java_dev_dload_NativeLoader_bootstrap` called.\n", .{});
+
+    // Warp the env into the wrapper.
+    _ = jni.JNIEnv.warp(env);
 }
